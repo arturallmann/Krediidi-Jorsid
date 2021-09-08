051ac400513aefe1df63e0a3420b9409e5639016
Artur Allmann <Artur.Allmann@tptlive.ee>
2021-09-08 11:42:27 +0300

    tegi js, html ja sql failid

---
+++ lepingud.sql
@@ -0,0 +1,10 @@
+Create TABLE Lepingud(
+    Id int AUTO_INCREMENT, 
+    Klient TEXT, 
+    Kliendi_Kontakt TEXT,
+    Summa FLOAT,
+    Intress INTEGER,
+    Viive INTEGER,
+    Solmimise_Kuupaev DATE,
+    PRIMARY KEY(Id)
+    )
