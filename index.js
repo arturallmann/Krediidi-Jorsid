3e38ca341d1aff94d1e24c2f2796add0d77d9466
Artur Allmann <Artur.Allmann@tptlive.ee>
2021-09-27 20:05:10 +0300

    lisasin andmebaasi jaoks index.js

--- index.js
+++ index.js
@@ -1,61 +1,15 @@
-import React, {useState} from "react";
-import {render} from "react-dom";
-import TextField from '@material-ui/core/TextField';
-import TextField from '@material-ui/core/Button';
+const { appendFileSync } = require("fs");
+var mysql = require("mysql");
 
-const CompundInterest = () => {
-    const[amount, setAmount] = useState();
-    const[weeks, setWeeks] = useState();
-    const[interest, setInterest] = useState();
-    const[result, setResult] = useState();
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Passw0rd",
+  database: "arvutitehnik",
+});
 
-    const calculate = () => {
-        // A=p(1+(r/n))^(nt)
-        const result = amount * Math.pow(1 + interest, weeks);
-        setResult(amount,toFixed(2 ))
-    };
-    return  (
-        <div
-            style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                height: "40vh",
-            }}>
-            <form>
-                <TextField 
-                    label="What is the amount?"
-                    variant="outlined"
-                    type="number"
-                    onChange={(e) => setAmount(e.target.value)}
-                />
-                <TextField
-                    label="How many weeks?"
-                    variant="outlined"
-                    onChange={(e) => setWeeks(e.targer.value)}
-                />
-                <TextField
-                    label="Weekly Interest rate?"
-                    variant="outlined"
-                    type="number"
-                    onChange={(e) => setInterest(e.targer.value / 100)}           
-                />
-                <br />
-                <br />
-                <Button
-                    variant="contained"
-                    color="primary"
-                    type="number"
-                    onClick={() => {
-                        calculate()
-                    }}>
-                    Calculate
-                </Button>
-                <br /><br />
-                <div style={{fontSize: "30px"}}>€{result}</div>
-            </form>
-        </div>
-    );
-};
-
-render(<CompundInterest />, document.querySelector("#root"));
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("You are now connected...");
+});
+app.listen(8080);
